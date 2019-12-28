bef5ddd4e15a831883724b6331b8895c262da686
Kedar Kshatriya <kshatriyakedar@gmail.com>
2019-12-29 04:55:33 +0530

    Updates

---
+++ precache-manifest.d3336d8a8a22c1fe079c0f45844bb6d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9076dfe2e3859fc172bbc780896120f6",
+    "url": "/com/index.html"
+  },
+  {
+    "revision": "a7ea8bbeb691f4e14823",
+    "url": "/com/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "682e3c2e736846a78766",
+    "url": "/com/static/js/2.0cb983a8.chunk.js"
+  },
+  {
+    "revision": "a1afa5f7c8dff2a316c41be2103336f5",
+    "url": "/com/static/js/2.0cb983a8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a7ea8bbeb691f4e14823",
+    "url": "/com/static/js/main.6bc8ebb8.chunk.js"
+  },
+  {
+    "revision": "fc0f4754276e18ac7e85",
+    "url": "/com/static/js/runtime-main.cbf3e320.js"
+  }
+]);
